8b6fa20ea17e2c97cc4b91a57a9cc17766359694
hnae6443 <hnae6443@gmail.com>
2015-10-07 10:02:05 -0400

    Added bars functionality, it works

--- app/models/yelp.rb
+++ app/models/yelp.rb
@@ -17,26 +17,42 @@ class Yelp
 		api_host = 'api.yelp.com'
 
 		@food_path = "/v2/search?term=food&ll=#{@array[0]},#{@array[1]}&limit=#{@array[2]}"
-
+		@bar_path = "/v2/search?term=bar&ll=#{@array[0]},#{@array[1]}&limit=#{@array[2]}"
 		consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
 		access_token = OAuth::AccessToken.new(consumer, token, token_secret)
 
-		@yelp_data = JSON.parse(access_token.get(@food_path).body)
+		@restaurant_data = JSON.parse(access_token.get(@food_path).body)
+		@bar_data = JSON.parse(access_token.get(@bar_path).body)
 	end
 
-	def returning
-		rest = [] 
+	def restaurant_yelp
+		restaurant = [] 
 		get_values
 		i = 0
 		while i < @array[2].to_i
-			rest.push(
-				@yelp_data["businesses"][i]["name"], 
-				@yelp_data["businesses"][i]["rating"],
-				@yelp_data["businesses"][i]["rating_img_url_small"],
-				@yelp_data["businesses"][i]["url"],
-				@yelp_data["businesses"][i]["image_url"])
+			restaurant.push(
+				@restaurant_data["businesses"][i]["name"], 
+				@restaurant_data["businesses"][i]["rating"],
+				@restaurant_data["businesses"][i]["rating_img_url_small"],
+				@restaurant_data["businesses"][i]["url"],
+				@restaurant_data["businesses"][i]["image_url"])
 			i += 1
 		end
-		rest
+		restaurant
+	end
+		def bar_yelp
+		bar = []
+		get_values
+		i = 0
+		while i < @array[2].to_i
+			bar.push(
+				@bar_data["businesses"][i]["name"], 
+				@bar_data["businesses"][i]["rating"],
+				@bar_data["businesses"][i]["rating_img_url_small"],
+				@bar_data["businesses"][i]["url"],
+				@bar_data["businesses"][i]["image_url"])
+			i += 1  	
+		end  
+		bar
 	end
 end
